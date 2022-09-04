1e808fc8a91a0bf06c4663f8fb9c9aef66c445c3
ACXYZ <47590087+notACXYZ@users.noreply.github.com>
2022-09-04 15:29:10 +0200

    Working SpamDB on vichan

--- templates/themes/spamdb/info.php
+++ templates/themes/spamdb/info.php
@@ -1,49 +1,53 @@
 <?php
-	$theme = Array();
-	
-	// Theme name
-	$theme['name'] = 'KolymaNET SpamDB';
-	// Description (you can use Tinyboard markup here)
-	$theme['description'] = 'Uses <a href="//spam.kolyma.org/spam.php">Kolyma Network Anti-Spam Database</a> and automatically bans the listed verified IP\'s at a custom length. An API key is required to use the service.';
-	$theme['version'] = 'v0.0.1';
-	
-	// Theme configuration	
-	$theme['config'] = Array();
-	
-	//Key
-	$theme['config'][] = Array(
-		'title' => 'API Key',
-		'name' => 'apikey',
-		'type' => 'text'
-	);
-	//Categories
-	$theme['config'][] = Array(
-		'title' => 'Category',
-		'name' => 'category',
-		'type' => 'text',
-		'comment' => '(eg. "PROXY_IP")'
-		//'size' => 3,
-	);
-	
-	$theme['config'][] = Array(
-		'title' => 'Ban Length',
-		'name' => 'ban_length',
-		'type' => 'text',
-		//'default' => $config['file_index'],
-		'comment' => '(eg. "2d1h30m" or "2 days")'
-	);
-	
-	// Unique function name for building everything
-	$theme['build_function'] = 'spamdb_build';
-	$theme['install_callback'] = 'spamdb_install';
+    $theme = Array();
+    
+    // Theme name
+    $theme['name'] = 'KolymaNET SpamDB Client';
+    // Description (you can use Tinyboard markup here)
+    $theme['description'] = 'Uses <a href="//spam.kolyma.org/spam.php">KolymaNET Anti-Spam Database</a> to automatically filter and block posts. An API key is required.';
+    $theme['version'] = 'v0.1.3';
+    
+    // Theme configuration    
+    $theme['config'] = Array();
+    
+    $theme['config'][] = Array(
+        'title' => 'API Key',
+        'name' => 'apikey',
+        'type' => 'text'
+    );
+    
+    $theme['config'][] = Array(
+        'title' => 'Verified Only',
+        'name' => 'verifonly',
+        'type' => 'checkbox',
+        'default' => true,
+        'comment' => '(verified anti-spam entries only)'
+    );
+    
+    $theme['config'][] = Array(
+        'title' => 'Timeout',
+        'name' => 'curl_timeout',
+        'type' => 'text',
+        'comment' => '(in seconds)',
+        'default' => '5',
+        'size' => '1'
+    );
+    
+    // Unique function name for building everything
+    $theme['build_function'] = 'spamdb_build';
+    $theme['install_callback'] = 'spamdb_install';
 
-	if (!function_exists('spamdb_install')) {
-		function spamdb_install($settings) {
-		/*	if (!is_numeric($settings['ban_length']) || $settings['ban_length'] < 0)
-				return Array(false, '<strong>' . utf8tohtml($settings['ban_length']) . '</strong> is not a non-negative integer.');
-		}*/
-			if (!is_numeric($settings['category']) == $settings['category'] < 0)
-				return Array(false, '<strong>' . utf8tohtml($settings['ban_length']) . '</strong> is not a non-negative integer.');
-		}
-	}
-
+    if (!function_exists('spamdb_install')) {
+        function spamdb_install($settings) {
+			
+			// Validate Timeout
+            if (!is_numeric($settings['curl_timeout']) || $settings['curl_timeout'] < 0)
+                return Array(false, '<strong>' . utf8tohtml($settings['curl_timeout']) . '</strong> is not a non-negative integer.');
+			
+			// cURL installed?
+			if (!function_exists('curl_version')){
+				return Array(false, '<strong>cURL</strong> is not installed.');
+			}
+			
+        }
+    }
